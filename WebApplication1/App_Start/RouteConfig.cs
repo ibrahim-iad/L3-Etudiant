f5c43bf04bbcbbd22ffef96659003e987f331905
ibroman <ibroman79@gmail.com>
2022-04-23 23:32:47 +0300

    Créer la controller personalisé "Calcul" et la vue "Index".

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace WebApplication1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Calcul",
+                url: "{controller}/{action}/{id1}/{id2}",
+                defaults: new { controller = "Calcul", action = "Index" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
---
+++ WebApplication1/Controllers/CalculController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculController : Controller
+    {
+        // GET: Calcul
+        public ActionResult Index(int id1, int id2)
+        {
+            CalculResultat model = new CalculResultat();
+            model.somme = id1 + id2;
+            model.difference = id1 - id2;
+            model.produit = id1 * id2;
+            model.division = Convert.ToSingle(id1) / id2;
+            return View(model);
+        }
+    }
+}
