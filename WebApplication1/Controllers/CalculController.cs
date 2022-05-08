dc99d925b69b0bdd96dfae5ea8cc5a602d0b9291
Ibroman <ibroman79@gmail.com>
2022-05-08 14:38:43 +0300

    Création de la branche "formulaires" : - création d'un formulaire de saisie pour le contrôleur "Calcul" - utilisation de Html Helpers dans un formulaire

--- WebApplication1/Controllers/CalculController.cs
+++ WebApplication1/Controllers/CalculController.cs
@@ -21,5 +21,40 @@ namespace WebApplication1.Controllers
             model.division = Convert.ToSingle(id1) / id2;
             return View(model);
         }
+
+        public ActionResult New()
+        {
+            CalculResultat model = new CalculResultat();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult New(CalculResultat model)
+        {
+            model.somme = model.nombre1 + model.nombre2;
+            model.difference = model.nombre1 - model.nombre2;
+            model.produit = model.nombre1 * model.nombre2;
+            model.division = Convert.ToSingle(model.nombre1) / model.nombre2;
+            return View("Index", model);            
+        }
+
+        public ActionResult New2()
+        {
+            CalculResultat model = new CalculResultat();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult New2(int nombre1, int nombre2)
+        {
+            CalculResultat model = new CalculResultat();
+            model.nombre1 = nombre1;
+            model.nombre2 = nombre2;
+            model.somme = nombre1 + nombre2;
+            model.difference = nombre1 - nombre2;
+            model.produit = nombre1 * nombre2;
+            model.division = Convert.ToSingle(nombre1) / nombre2;
+            return View("Index", model);
+        }
     }
 }
--- WebApplication1/Models/CalculResultat.cs
+++ WebApplication1/Models/CalculResultat.cs
@@ -7,11 +7,11 @@ namespace WebApplication1.Models
 {
     public class CalculResultat
     {
-        public int nombre1;
-        public int nombre2;
-        public int somme;
-        public int difference;
-        public int produit;
-        public float division;
+        public int nombre1 { get; set; }
+        public int nombre2 { get; set; }
+        public int somme { get; set; }
+        public int difference { get; set; }
+        public int produit { get; set; }
+        public float division { get; set; }
     }
 }
